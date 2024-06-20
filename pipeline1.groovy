631105eb8ebf621fb36af2f562b2ce6bfd4ee4c5
durga97176 <70195793+durga97176@users.noreply.github.com>
2024-06-20 10:47:40 -0400

    Update pipeline1.groovy

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                echo 'Build application code'
+                sh 'mvn package -f pox.xml'
             }
         }
         stage('Deploy') {

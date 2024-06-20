f10ca162e3b500293693a67499ac720b347f0d44
durga97176 <70195793+durga97176@users.noreply.github.com>
2024-06-20 11:32:08 -0400

    Update pipeline1.groovy

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -1,8 +1,8 @@
 pipeline {
     agent any
-    parameters{
-        choice{name:'VERSION',choice:['1.0','1.1','1.2']}
-        boolenparam(name: 'durga',defaultValue: true)
+    parameters {
+        choice(name: 'VERSION', choices: ['1.0', '1.1', '1.2'])
+        booleanParam(name: 'durga', defaultValue: true)
     }
 
     stages {
@@ -11,20 +11,28 @@ pipeline {
                 git 'https://github.com/durga97176/simple-java-maven-app.git'
             }
         }
+
         stage('Build') {
             steps {
-                script {
-                    if (isUnix()) {
-                        sh 'mvn package -f pom.xml'
-                    } else {
-                        bat 'mvn package -f pom.xml'
-                    }
-                }
+                sh 'mvn package -f pom.xml'
             }
         }
+
+        stage('Upload to Nexus') {
+            steps {
+                nexusArtifactUploader credentialsId: 'nexuskey', 
+                                     groupId: 'com.mycompany.app', 
+                                     nexusUrl: 'http://54.167.126.21:8081/nexus', 
+                                     nexusVersion: 'nexus2', 
+                                     protocol: 'http', 
+                                     repository: 'releases', 
+                                     version: '1.0-SNAPSHOT'
+            }
+        }
+
         stage('Deploy') {
             steps {
-                echo 'deployed the application code'
+                echo 'Deployed the application code'
             }
         }
     }
